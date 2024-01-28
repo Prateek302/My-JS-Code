04bf1f93de503b16bf051af325079f76a7be9d76
prateek <prateeksrivastav333@gmail.com>
2024-01-28 22:09:56 +0530

    inheritance

---
+++ 10_oop/inheritance.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`USSERNAME is ${this.username}`);
+    }
+
+}
+
+class Teacher extends User {
+    constructor(username, email, password){
+        super(username)
+        this.email = email
+        this.password = password
+    }
+
+    addCourse(){
+        console.log(`A new course was added by ${this.username}`);
+    }
+}
+
+const chai = new Teacher("chai", "chai@teacher.com", "123")
+
+chai.addCourse()
+
+const masalaChai = new User ("masalaChai")
+
+masalaChai.logMe()
+
+console.log(chai instanceof Teacher);
